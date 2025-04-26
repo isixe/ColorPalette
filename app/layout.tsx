77b4ebac80e1f148adebd5980833825addff04c4
isixe <isixe@outlook.com>
2025-04-26 18:41:26 +0800

    🎈 perf(app): add keywords, canonical and openGraph metadata for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,32 @@ import Header from '@/components/layout/header'
 import { Toaster } from '@/components/ui/toaster'
 import '@/styles/global.css'
 import type { Metadata } from 'next'
+import { headers } from 'next/headers'
 
-export const metadata: Metadata = {
-  title: 'Color Palette',
-  description: 'Color Palette Tools'
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers()
+  const protocol = headersList.get('x-forwarded-proto') || 'http'
+  const host = headersList.get('host') || 'localhost:3000'
+  const url = `${protocol}://${host}`
+
+  return {
+    title: 'Color Palette',
+    keywords:
+      'color, colors, color palette, color picker, color generator, color palette generator, color tool',
+    description:
+      'ColorPalette is a tool designed to help developers and designers create, manage, and use color palettes efficiently.',
+    alternates: {
+      canonical: url
+    },
+    openGraph: {
+      title: 'Color Palette',
+      description:
+        'ColorPalette is a tool designed to help developers and designers create, manage, and use color palettes efficiently.',
+      url,
+      siteName: 'Color Palette',
+      locale: 'en'
+    }
+  }
 }
 
 export default function RootLayout({
