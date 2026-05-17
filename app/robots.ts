fbd0037b2c9fdfaddb7c8c1c2621ffcee928e6a6
isixe <isixe@outlook.com>
2026-05-17 12:18:21 +0800

    ✨ feat(robots): Add dynamic robots.txt generation.

---
+++ app/robots.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+import { headers } from 'next/headers'
+
+export const runtime = 'edge'
+
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  const headersList = await headers()
+  const protocol = headersList.get('x-forwarded-proto')
+  const host = headersList.get('host')
+  const baseUrl = `${protocol}://${host}`
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/_next/*']
+      },
+      {
+        userAgent: 'meta-externalagent',
+        disallow: '/'
+      },
+      {
+        userAgent: 'Amazonbot',
+        disallow: '/'
+      }
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`
+  }
+}
